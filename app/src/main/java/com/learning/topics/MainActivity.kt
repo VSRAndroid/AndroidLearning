d360670f86126126e9d42b35447b6a975f7f2cf2
Vikrant Rawat <vickyrawat.rawat6@gmail.com>
2023-04-22 21:31:01 +0530

    learn async and suspend function

--- app/src/main/java/com/learning/topics/MainActivity.kt
+++ app/src/main/java/com/learning/topics/MainActivity.kt
@@ -2,10 +2,36 @@ package com.learning.topics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(IO).launch {
+            val firstValue = async { firstTask() }
+            val secondValue = async { secondTask() }
+
+            val totalValue = firstValue.await() + secondValue.await()
+            Log.e("MainActivity Total Value ", "Is "+ totalValue)
+        }
+    }
+
+    suspend fun firstTask() : Int{
+        delay(10000)
+        Log.e("MainActivity First Task ","Is Running.")
+        return 5000
+    }
+
+    suspend fun secondTask() : Int{
+        delay(8000)
+        Log.e("MainActivity Second Task ","Is Running.")
+        return 700
     }
 }
