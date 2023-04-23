31b8535ab80ab2667b7875cb1c29c63f27a7c2ed
Vikrant Rawat <vickyrawat.rawat6@gmail.com>
2023-04-23 21:44:14 +0530

    learn structure concurrency function

--- app/src/main/java/com/learning/topics/MainActivity.kt
+++ app/src/main/java/com/learning/topics/MainActivity.kt
@@ -1,29 +1,46 @@
 package com.learning.topics
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    var countTextView:TextView ?= null
+    var  clickBtn: Button?= null
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        CoroutineScope(IO).launch {
+        clickBtn = findViewById(R.id.clickBtn)
+        countTextView = findViewById(R.id.countTextView)
+        // below code for learn async and await
+        /*CoroutineScope(IO).launch {
             val firstValue = async { firstTask() }
             val secondValue = async { secondTask() }
 
             val totalValue = firstValue.await() + secondValue.await()
             Log.e("MainActivity Total Value ", "Is "+ totalValue)
-        }
+        }*/
+
+        clickBtn?.setOnClickListener(View.OnClickListener {
+            CoroutineScope(Main).launch {
+                countTextView?.text = UserDataManager().getTotalValue().toString()
+                Log.e("get value",".."+UserDataManager().getTotalValue().toString())
+            }
+        })
     }
 
-    suspend fun firstTask() : Int{
+  /*  suspend fun firstTask() : Int{
         delay(10000)
         Log.e("MainActivity First Task ","Is Running.")
         return 5000
@@ -33,5 +50,5 @@ class MainActivity : AppCompatActivity() {
         delay(8000)
         Log.e("MainActivity Second Task ","Is Running.")
         return 700
-    }
+    }*/
 }
---
+++ app/src/main/java/com/learning/topics/UserDataManager.kt
@@ -0,0 +1,25 @@
+package com.learning.topics
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class UserDataManager {
+
+    suspend fun getTotalValue() : Int{
+        var count = 0
+        CoroutineScope(IO).launch {
+            delay(1000)
+            count = 100
+        }
+
+         val getValue = CoroutineScope(IO).async {
+            delay(10000)
+            return@async 100
+        }
+
+        return  count + getValue.await()
+    }
+}
